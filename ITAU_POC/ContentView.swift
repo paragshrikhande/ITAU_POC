01a03880138ce7ec88a26c6088082134ad97db63
paragshrikhande <shrikhande01@gmail.com>
2019-11-05 15:22:09 +0530

    Initial Commit

---
+++ ITAU_POC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ITAU_POC
+//
+//  Created by Parag on 05/11/19.
+//  Copyright © 2019 Parag. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ITAU_POCTests/ITAU_POCTests.swift
@@ -0,0 +1,34 @@
+//
+//  ITAU_POCTests.swift
+//  ITAU_POCTests
+//
+//  Created by Parag on 05/11/19.
+//  Copyright © 2019 Parag. All rights reserved.
+//
+
+import XCTest
+@testable import ITAU_POC
+
+class ITAU_POCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
